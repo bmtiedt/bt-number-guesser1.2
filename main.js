d90b3ed85f714fea3dcdd7f86d34dcf7860a08e1
Brian Tiedt <bmtiedt@gmail.com.com>
2018-05-13 16:33:19 -0600

    Add Js functions and header for HTML

--- main.js
+++ main.js
@@ -14,29 +14,36 @@ guessButton.addEventListener('click', function() {
   submitGuess();
 });
 
+function rangeExpand(n) {
+  min = min - n;
+  max = max + n;
+}
+
 function submitGuess() {
+  var feedback = document.getElementById('js-message');
   document.querySelector('.reset').disabled = false;
   document.getElementById('instruction').innerText = 'Your previous guess was...';
-  if (parseInt(randomGuess.value) === randomNumber) {   
+  if (parseInt(randomGuess.value) === randomNumber) {
+    rangeExpand(10);  
     randomNumber = generateRandomNumber();
-    return document.getElementById('js-message').innerText = 'Oh SNAP!!!!';
+    feedback.innerText = '**^***Oh SNAP!!!!***^**';
   }
   if (parseInt(randomGuess.value) < min || parseInt(randomGuess.value) > max || isNaN(parseInt(randomGuess.value))) {
     document.getElementById('instruction').innerText = 'This number is not within range';
-      return document.getElementById('js-message').innerText = 'Please choose a valid number!';
+    feedback.innerText = ' ¯\_(ツ)_/¯ Please choose a valid number!';
   }
   if (parseInt(randomGuess.value) > randomNumber) {
-    document.getElementById('js-message').innerText = 'Sorry, that guess is too high. Try a lower number.';
-      return document.getElementById('numberGuess').value='';
+    feedback.innerText = 'Sorry, that guess is too high. Try a lower number.';
+    document.getElementById('numberGuess').value='';
   }
   if (parseInt(randomGuess.value) < randomNumber) {
-    document.getElementById('js-message').innerText = 'Sorry, that guess is too low. Try a higher number.';
-      return document.getElementById('numberGuess').value='';
+    feedback.innerText = 'Sorry, that guess is too low. Try a higher number.';
+    document.getElementById('numberGuess').value='';
   }
 };
 
 function clearNumber() {
-  document.getElementById('numberGuess').value = '';
+  document.querySelector('#numberGuess').value = '';
 }
 
 function clearFields() {
@@ -57,7 +64,6 @@ function resetNumber() {
   generateRandomNumber();
 }
 
-//PLAYERS RANGE
 function submitUserNumbers() {
   document.getElementById('instruction').innerText = 'Make a guess between your selected numbers';
   clearFields();
@@ -70,7 +76,6 @@ function submitUserNumbers() {
   randomNumber = generateRandomNumber();
 }
 
-//ENABLE VREAL AND RESET BUTTON WITH INPUT
 function enableBtn() {
   document.getElementById('numberGuess').value != '';
   document.querySelector('.clear').disabled = false;
